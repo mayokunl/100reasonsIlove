d417740e052a6dfcae9fb034f64d3504fc466908
mayokunl <153923029+mayokunl@users.noreply.github.com>
2025-05-20 21:02:21 -0500

    push

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const reasons = [
         "9. You've become one of my favorite people, (now i know how much quality time acc means to me) ", 
         "10. You've kept me captivated since the day ive met you and theres never a dull moment",
         "11. Your patience, (thank you for always putting up with me) ",
+        "12. Your hardwork , your the definition of doubling your hussle , and u remind to always dounle mine ",
         ":( you have reached the end, come back tomorrow for a new reason",
     ];
 
